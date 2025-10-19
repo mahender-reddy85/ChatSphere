d4f93f0964321a6fbbf9df1d29101627974dbfa2
SSMM-25 <teambankai2025@gmail.com>
2025-10-19 19:29:15 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
   theme: {
