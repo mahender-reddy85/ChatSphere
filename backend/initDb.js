90debe2ea13b6af4c2eaa3dce713af825b736ba3
Likki Mahender Reddy <likkimahenderreddy123@gmail.com>
2026-01-10 21:24:42 +0530

    .

--- backend/initDb.js
+++
@@ -1,3 +0,0 @@
-// initDb removed — database is not used in development mode.
-// If you later need to re-enable DB, restore original init script.
-console.log('initDb script disabled: database is not used currently.');
